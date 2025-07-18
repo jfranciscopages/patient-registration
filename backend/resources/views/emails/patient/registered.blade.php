8dd3b88ced8631e6c3b02575b4f50e243668d30c
Francisco <zextfpages@correoargentino.com.ar>
2025-07-18 16:12:52 -0300

    ux fix

--- backend/resources/views/emails/patient/registered.blade.php
+++ backend/resources/views/emails/patient/registered.blade.php
@@ -1,17 +1,17 @@
 <x-mail::message>
-# ¡Hola {{ $patient->full_name }}!
+# Hello {{ $patient->full_name }}!
 
-Gracias por registrarte en nuestro sistema. Este es un mensaje de confirmación de que tu registro fue exitoso.
+Thank you for registering in our system. This is a confirmation message that your registration was successful.
 
 <x-mail::panel>
 **Email:** {{ $patient->email }}
-**Teléfono:** {{ $patient->phone_country_code }} {{ $patient->phone_number }}
+**Phone:** {{ $patient->phone_country_code }} {{ $patient->phone_number }}
 </x-mail::panel>
 
 <x-mail::button :url="'http://localhost:8000'">
-Ver sitio
+View site
 </x-mail::button>
 
-Gracias,<br>
+Thank you,<br>
 {{ config('app.name') }}
 </x-mail::message>
